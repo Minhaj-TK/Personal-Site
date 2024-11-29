44f977350669882de9b94033cfa3df2f09a3232d
Minhaj <112554910+Minhaj-TK@users.noreply.github.com>
2024-11-29 20:33:59 +0530

    Minhaj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
 
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
